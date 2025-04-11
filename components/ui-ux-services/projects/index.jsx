f67398ceae366511cbbfeb722feb5cd919b3161c
Anmol Kumar <anmolbuddy3@gmail.com>
2025-04-11 20:42:27 +0530

    hero section and scroll to top changes

--- components/ui-ux-services/projects/index.jsx
+++ components/ui-ux-services/projects/index.jsx
@@ -6,38 +6,11 @@ import Project3Img from "../../../public/images/project/project3/image2.png";
 import Project4Img from "../../../public/images/project/project4/image3.png";
 import Project5Img from "../../../public/images/project/project5/image4.png";
 import Project6Img from "../../../public/images/project/project6/image5.png";
-// import Project1Img from "../../../public/images/v6/p_1.png";
-// import Project2Img from "../../../public/images/v6/p_2.png";
-// import Project3Img from "../../../public/images/v6/p_3.png";
-// import Project4Img from "../../../public/images/v6/p_4.png";
 import Shape2Img from "../../../public/images/v6/shapes2.png";
 import ProjectCard from "./ProjectCard";
 
 const projectsData = [
-	// {
-	// 	id: crypto.randomUUID(),
-	// 	title: "Corporate Elegance",
-	// 	img: Project1Img,
-	// 	colSize: "6",
-	// },
-	// {
-	// 	id: crypto.randomUUID(),
-	// 	title: "Corporate Elegance",
-	// 	img: Project2Img,
-	// 	colSize: "6",
-	// },
-	// {
-	// 	id: crypto.randomUUID(),
-	// 	title: "Corporate Elegance",
-	// 	img: Project3Img,
-	// 	colSize: "8",
-	// },
-	// {
-	// 	id: crypto.randomUUID(),
-	// 	title: "Corporate Elegance",
-	// 	img: Project4Img,
-	// 	colSize: "4",
-	// },
+	
 	{
 			id: crypto.randomUUID(),
 			title: "Taylor Tasting Tours",
